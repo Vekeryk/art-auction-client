1c972164fa4097aba2fc74481a2480160267cd4c
Denys Vekeryk <den.vekeryk@gmail.com>
2024-05-29 23:57:35 +0300

    fix(bidding): lot control

--- src/components/lot/LotDetails/index.tsx
+++ src/components/lot/LotDetails/index.tsx
@@ -5,14 +5,16 @@ import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 import GroupsIcon from '@mui/icons-material/Groups';
 import GavelIcon from '@mui/icons-material/Gavel';
 import SellIcon from '@mui/icons-material/Sell';
+import NotInterestedIcon from '@mui/icons-material/NotInterested';
 
 import Timer from '../../common/Timer';
 import CategoryAndTags from '../../common/CategoryAndTags';
 import IconContainer from '../../common/IconContainer';
 import { authenticatedAction } from '../../../utils/users.ts';
-import { Lot } from '../../../types.ts';
+import { Lot, LotStatus } from '../../../types.ts';
 import LotReportAction from '../LotReportAction';
 import useBidding from '../../../hooks/useBidding.ts';
+import useAppSelector from '../../../hooks/useAppSelector.ts';
 
 interface ILotDetails {
   lot: Lot;
@@ -27,6 +29,7 @@ const LotDetails: React.FC<ILotDetails> = ({ lot, preview }) => {
     handlePlaceBid,
     usersCount,
   } = useBidding(preview ? null : lot);
+  const user = useAppSelector((state) => state.userReducer.user);
 
   return (
     <Stack spacing={2} flexGrow={1}>
@@ -54,27 +57,42 @@ const LotDetails: React.FC<ILotDetails> = ({ lot, preview }) => {
             </IconContainer>
           )}
         </Stack>
-        <Stack component="form" direction="row" spacing={2} width="100%">
-          <TextField
-            label="Ваша ставка"
-            type="number"
-            value={userBidAmount}
-            onChange={(e) => setUserBidAmount(+e.target.value)}
-            InputProps={{
-              inputProps: { min: lot.currentPrice, max: 100000000 },
-            }}
-          />
-          <Button
-            onClick={() => authenticatedAction(handlePlaceBid)}
-            variant="contained"
-            color="primary"
-            startIcon={<GavelIcon />}
-          >
-            Зробити ставку
-          </Button>
-        </Stack>
+        {lot.status === LotStatus.ACTIVE && lot.user.id !== user?.id && (
+          <Stack component="form" direction="row" spacing={2} width="100%">
+            <TextField
+              label="Ваша ставка"
+              type="number"
+              value={userBidAmount}
+              onChange={(e) => setUserBidAmount(+e.target.value)}
+              InputProps={{
+                inputProps: { min: lot.currentPrice, max: 100000000 },
+              }}
+            />
+            <Button
+              onClick={() => authenticatedAction(handlePlaceBid)}
+              variant="contained"
+              color="primary"
+              startIcon={<GavelIcon />}
+            >
+              Зробити ставку
+            </Button>
+          </Stack>
+        )}
       </Stack>
-      <LotReportAction />
+      <Stack justifyContent="right" flexWrap="wrap" direction="row" spacing={1}>
+        {lot.user.id === user?.id && (
+          <Button
+            onClick={() => authenticatedAction(() => {})}
+            variant="text"
+            color="error"
+            sx={{ alignSelf: 'end', justifySelf: 'end' }}
+            startIcon={<NotInterestedIcon />}
+          >
+            Зняти з торгів
+          </Button>
+        )}
+        <LotReportAction />
+      </Stack>
     </Stack>
   );
 };
